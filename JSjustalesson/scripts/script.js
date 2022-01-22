5653381e2cd7aa56093033e5bebfb9141f9a7b8b
Unr1lov <>
2022-01-22 10:56:26 +0300

    update valanta

---
+++ JSjustalesson/scripts/script.js
@@ -0,0 +1,30 @@
+//Задание 1
+let topSpeed = 160;
+let distance = 617.54;
+let login = "mango935";
+let isOnline = true;
+let isAdmin = false;
+console.log(topSpeed);
+console.log(distance);
+console.log(login);
+console.log(isOnline);
+console.log(isAdmin);
+//Задание 2
+console.log(topSpeed + " " + distance + " " + login + " " + isOnline + " " + isAdmin);
+//Задание 3
+let Hello = "Hello, ";
+let World = "world";
+console.log(Hello + World);
+//Задание 4
+let a = 11;
+let b = 756;
+let sum = a + b;
+let message = `Сумма чисел ${a} и ${b}: ${sum}`;
+console.log(message);
+//Задание 5
+let pricePerDroid = 800; //Цена
+let orderedQuatity = 6; //Количество
+let deliveryFee = 50;  //Стоимость доставки
+let totalPrice = (pricePerDroid * orderedQuatity) + deliveryFee;
+let message1 = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+console.log(message1);
